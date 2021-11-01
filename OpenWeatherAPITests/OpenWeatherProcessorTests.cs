7d4be6f2c16805aa2c4a39b4759bee78c0b88884
Mathieu <1931721@cshawi.ca>
2021-11-01 18:45:47 -0400

    T1 - Complété - 1931721

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -1,9 +1,21 @@
-﻿using System;
+﻿using OpenWeatherAPI;
+using System;
+using Xunit;
 
 namespace OpenWeatherAPITests
 {
     public class OpenWeatherProcessorTests
     {
+
+        [Fact]
+        public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+            OpenWeatherProcessor owp = OpenWeatherProcessor.Instance;
+
+            Assert.ThrowsAsync<ArgumentException>(owp.GetOneCallAsync);
+
+        }
+
 
     }
 }
