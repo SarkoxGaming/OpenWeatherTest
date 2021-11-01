b4dac68e8f9788663e8d83e6bbf4511009d42ac2
Mathieu <1931721@cshawi.ca>
2021-11-01 19:04:40 -0400

    T3 - Complété - 1931721 T4 - Complété - 1931721

--- OpenWeatherAPITests/OpenWeatherProcessorTests.cs
+++ OpenWeatherAPITests/OpenWeatherProcessorTests.cs
@@ -11,12 +11,31 @@ namespace OpenWeatherAPITests
         public void GetOneCallAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
         {
             OpenWeatherProcessor owp = OpenWeatherProcessor.Instance;
+            ApiHelper.InitializeClient();
 
             Assert.ThrowsAsync<ArgumentException>(owp.GetOneCallAsync);
+
         }
 
         [Fact]
         public void GetCurrentWeatherAsync_IfApiKeyEmptyOrNull_ThrowArgumentException()
+        {
+            OpenWeatherProcessor owp = OpenWeatherProcessor.Instance;
+            ApiHelper.InitializeClient();
+
+            Assert.ThrowsAsync<ArgumentException>(owp.GetCurrentWeatherAsync);
+        }
+
+        [Fact]
+        public void GetOneCallAsync_IfApiHelperNotInitialized_ThrowArgumentException()
+        {
+            OpenWeatherProcessor owp = OpenWeatherProcessor.Instance;
+            
+            Assert.ThrowsAsync<ArgumentException>(owp.GetOneCallAsync);
+        }
+
+        [Fact]
+        public void GetCurrentWeatherAsync_IfApiHelperNotInitialized_ThrowArgumentException()
         {
             OpenWeatherProcessor owp = OpenWeatherProcessor.Instance;
 
